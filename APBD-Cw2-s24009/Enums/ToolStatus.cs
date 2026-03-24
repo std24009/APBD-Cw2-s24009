fcc7401cb311bf48a22135c21d49eca54cc77c75
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-24 17:32:58 +0100

    add base tool class

---
+++ APBD-Cw2-s24009/Enums/ToolStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Cw2_s24009.Enums;
+
+public enum ToolStatus
+{
+    Available,
+    Unavailable
+}
---
+++ APBD-Cw2-s24009/Models/WorkTool.cs
@@ -0,0 +1,12 @@
+﻿using APBD_Cw2_s24009.Enums;
+
+namespace APBD_Cw2_s24009.Models;
+
+public abstract class WorkTool(string name)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; } = _nextId++;
+    public string Name { get; set; } = name;
+    public ToolStatus Status { get; set; } = ToolStatus.Available;
+}

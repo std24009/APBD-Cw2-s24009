af97abe524ae47dfc92d7fbf90a279fae4736226
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-24 17:34:36 +0100

    add user types: (Student, Employee)

---
+++ APBD-Cw2-s24009/Models/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw2_s24009.Models;
+
+public class Employee(string fName, string lName) : User(fName, lName)
+{
+    public override int GetMaxReservations() => 5;
+}
---
+++ APBD-Cw2-s24009/Models/Student.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw2_s24009.Models;
+
+public class Student(string fName, string lName) : User(fName, lName)
+{
+    public override int GetMaxReservations() => 2;
+}

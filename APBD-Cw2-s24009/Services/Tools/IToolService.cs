85c37f7a9d489dbeaf5604966147c740970d58a9
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-24 18:34:06 +0100

    add ToolService and IToolService interface

---
+++ APBD-Cw2-s24009/Services/Tools/IToolService.cs
@@ -0,0 +1,13 @@
+﻿using APBD_Cw2_s24009.Models;
+
+namespace APBD_Cw2_s24009.Services.Tools;
+
+public interface IToolService
+{
+    public void AddTool(WorkTool workTool);
+    public WorkTool GetToolById(int toolId);
+    public List<WorkTool> GetAll();
+    public List<WorkTool> GetAvailable();
+    public void SetAvailable(int toolId);
+    public void SetUnavailable(int toolId);
+}
---
+++ APBD-Cw2-s24009/Services/Tools/ToolService.cs
@@ -0,0 +1,39 @@
+﻿using APBD_Cw2_s24009.Enums;
+using APBD_Cw2_s24009.Models;
+
+namespace APBD_Cw2_s24009.Services.Tools;
+
+public class ToolService : IToolService
+{
+    private readonly List<WorkTool> _tools = [];
+
+    public void AddTool(WorkTool workTool)
+    {
+        _tools.Add(workTool);
+    }
+
+    public WorkTool GetToolById(int toolId)
+    {
+        return _tools.Find(tool => tool.Id == toolId) ?? throw new Exception("Not found room: " + toolId);
+    }
+
+    public List<WorkTool> GetAll()
+    {
+        return _tools;
+    }
+
+    public List<WorkTool> GetAvailable()
+    {
+        return _tools.Where(tool => tool.Status == ToolStatus.Available).ToList();
+    }
+
+    public void SetAvailable(int toolId)
+    {
+        GetToolById(toolId).Status = ToolStatus.Available;
+    }
+
+    public void SetUnavailable(int toolId)
+    {
+        GetToolById(toolId).Status = ToolStatus.Unavailable;
+    }
+}

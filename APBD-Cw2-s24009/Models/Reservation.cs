0fdd3e43bd716bc712c6a8c22f6f7fea7210bfd1
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-24 18:16:50 +0100

    add reservation class

---
+++ APBD-Cw2-s24009/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD_Cw2_s24009.Models;
+
+public class Reservation(WorkTool workTool, User user, DateTime from, DateTime to)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; set; } = _nextId++;
+    public WorkTool WorkTool { get; set; } = workTool;
+    public User User { get; set; } = user;
+    public DateTime From { get; set; } = from >= to ? throw new ArgumentException("Invalid date") : from;
+    public DateTime To { get; set; } = to;
+    
+}

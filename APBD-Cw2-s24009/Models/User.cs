de10e06292f0788ecfbdea75262f091ef3033610
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-24 17:33:40 +0100

    add base User class

---
+++ APBD-Cw2-s24009/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_Cw2_s24009.Models;
+
+public abstract class User(string fName, string lName)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; set; } = _nextId++;
+    public string FName { get; set; } = fName;
+    public string LName { get; set; } = lName;
+
+    public abstract int GetMaxReservations();
+}

10e92d0c2f32830c836e927ad0443d870880a1e8
Marcin Duch <s24009@pjwstk.edu.pl>
2026-03-25 21:14:56 +0100

    fix reservation model

--- APBD-Cw2-s24009/Models/Reservation.cs
+++ APBD-Cw2-s24009/Models/Reservation.cs
@@ -9,5 +9,16 @@ public class Reservation(WorkTool workTool, User user, DateTime from, DateTime t
     public User User { get; set; } = user;
     public DateTime From { get; set; } = from >= to ? throw new ArgumentException("Invalid date") : from;
     public DateTime To { get; set; } = to;
-    
+    public bool IsCancelled { get; private set; } = false;
+
+    public void Cancel()
+    {
+        IsCancelled = true;
+    }
+
+    public bool Overlaps(DateTime from, DateTime to)
+    {
+        return !(From > to || from > To);
+    }
+
 }
